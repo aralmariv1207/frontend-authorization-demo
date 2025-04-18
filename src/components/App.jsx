c056bf662622d75eabd14d906eed8a60c32e6b30
aralmariv1207 <almariv.ar1207@outlook.com>
2025-04-17 23:36:02 -0400

    Update JSX and JS files and folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
+import AppContext from "./AppContext";
 
 import * as auth from "../utils/auth";
 import * as api from "../utils/api";
@@ -14,6 +21,49 @@ import { setToken } from "../utils/token";
 import "./styles/App.css";
 
 function App() {
+  const [userData, setUserData] = useState({ username: "", email: "" });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const handleRegistration = ({
+    username,
+    email,
+    password,
+    confirmPassword,
+  }) => {
+    if (password !== confirmPassword) {
+      auth
+        .register(username, password, email)
+        .then(() => {
+          navigate("/login");
+        })
+        .catch(console.error);
+    }
+  };
+
+  const handleLogin = ({ username, password }) => {
+    if (!username || !password) {
+      return;
+    }
+
+    auth
+      .authorize(username, password)
+      .then((data) => {
+        if (data.jwt) {
+          setToken(data.jwt);
+          setUserData(data.user);
+
+          setIsLoggedIn(true);
+          const redirectPath = location.state?.from?.pathname || "/ducks";
+          navigate(redirectPath);
+        }
+      })
+      .catch(console.error);
+  };
+
   useEffect(() => {
     const jwt = setToken();
     if (!jwt) {
@@ -28,84 +78,65 @@ function App() {
       })
       .catch(console.error);
   }, []);
-  const [userData, setUserData] = useState({ username: "", email: "" });
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const navigate = useNavigate();
-
-  const handleLogin = ({ username, password }) => {
-    if (!username || !password) {
-      return;
-    }
-
-    auth
-      .authorize(username, password)
-      .then((data) => {
-        if (data.jwt) {
-          setToken(data.jwt);
-          setUserData(data.user);
-          localStorage.setItem("jwt", data.jwt);
-          setIsLoggedIn(true);
-          navigate("/ducks");
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const handleRegistration = ({
-    username,
-    email,
-    password,
-    confirmPassword,
-  }) => {
-    if (password !== confirmPassword) {
-      auth
-        .register(username, password, email)
-        .then((data) => {
-          navigate("/login");
-        })
-        .catch(console.error);
-    }
-  };
 
   return (
-    <Routes>
-      <Route
-        path="/ducks"
-        element={
-          <ProtectedRoute isLoggedIn={isLoggedIn}>
-            <Ducks />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/my-profile"
-        element={
-          <ProtectedRoute isLoggedIn={isLoggedIn}>
-            <MyProfile userData={userData} />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/login" element={<Login handleLogin={handleLogin} />} />
-      <Route
-        path="/register"
-        element={
-          <div className="registerContainer">
-            <Register handleRegistration={handleRegistration} />
-          </div>
-        }
-      />
-      <Route
-        path="*"
-        element={
-          isLoggedIn ? (
-            <Navigate to="/ducks" />
-          ) : (
-            <Navigate to="/login" replace />
-          )
-        }
-      />
-    </Routes>
+    <AppContext.Provider value={{ isLoggedIn }}>
+      <Routes>
+        <Route
+          path="/ducks"
+          element={
+            <ProtectedRoute>
+              <Ducks setIsLoggedIn={setIsLoggedIn} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-profile"
+          element={
+            <ProtectedRoute>
+              <MyProfile userData={userData} setIsLoggedIn={setIsLoggedIn} />
+            </ProtectedRoute>
+          }
+        />
+        {/* Wrap our /login route in a ProtectedRoute. Make sure to 
+      specify the anoymous prop, to redirect logged-in users 
+      to "/". */}
+        <Route
+          path="/login"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn} anonymous>
+              <div className="loginContainer">
+                <Login handleLogin={handleLogin} />
+              </div>
+            </ProtectedRoute>
+          }
+        />
+        {/* Wrap our /register route in a ProtectedRoute. Make sure to
+      specify the anoymous prop, to redirect logged-in users 
+      to "/". */}
+        <Route
+          path="/register"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn} anonymous>
+              <div className="registerContainer">
+                <Register handleRegistration={handleRegistration} />
+              </div>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            isLoggedIn ? (
+              <Navigate to="/ducks" replace />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+      </Routes>
+    </AppContext.Provider>
   );
 }
 
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,23 @@
-import { Navigate } from "react-router-dom";
+// ProtectedRoute.jsx
 
-function ProtectedRoute({ isLoggedIn, children }) {
-  if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
+import { useContext } from "react"; // New import
+import { Navigate, useLocation } from "react-router-dom";
+
+// Remove isLoggedIn from props
+export default function ProtectedRoute({ children, anonymous = false }) {
+  const location = useLocation();
+  const from = location.state?.from || "/";
+
+  // Destructure isLoggedIn from the value provided by AppContext
+  const { isLoggedIn } = useContext(AppContext);
+
+  if (anonymous && isLoggedIn) {
+    return <Navigate to={from} />;
   }
+
+  if (!anonymous && !isLoggedIn) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
   return children;
 }
-
-export default ProtectedRoute;
