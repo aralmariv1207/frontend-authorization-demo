c58dacc468500ce79f6fac01ce8b274b5f2ad97d
aralmariv1207 <almariv.ar1207@outlook.com>
2025-04-17 22:28:35 -0400

    Update JSX and JS files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,50 @@ import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
 
 import * as auth from "../utils/auth";
+import * as api from "../utils/api";
+
+import { setToken } from "../utils/token";
 
 import "./styles/App.css";
 
 function App() {
+  useEffect(() => {
+    const jwt = setToken();
+    if (!jwt) {
+      return;
+    }
+    api
+      .getUserInfo(jwt)
+      .then(({ username, email }) => {
+        setIsLoggedIn(true);
+        setUserData({ username, email });
+        navigate("/ducks");
+      })
+      .catch(console.error);
+  }, []);
+  const [userData, setUserData] = useState({ username: "", email: "" });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const navigate = useNavigate();
+
+  const handleLogin = ({ username, password }) => {
+    if (!username || !password) {
+      return;
+    }
+
+    auth
+      .authorize(username, password)
+      .then((data) => {
+        if (data.jwt) {
+          setToken(data.jwt);
+          setUserData(data.user);
+          localStorage.setItem("jwt", data.jwt);
+          setIsLoggedIn(true);
+          navigate("/ducks");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleRegistration = ({
     username,
@@ -24,7 +61,7 @@ function App() {
     if (password !== confirmPassword) {
       auth
         .register(username, password, email)
-        .then(() => {
+        .then((data) => {
           navigate("/login");
         })
         .catch(console.error);
@@ -45,18 +82,11 @@ function App() {
         path="/my-profile"
         element={
           <ProtectedRoute isLoggedIn={isLoggedIn}>
-            <MyProfile />
+            <MyProfile userData={userData} />
           </ProtectedRoute>
         }
       />
-      <Route
-        path="/login"
-        element={
-          <div className="loginContainer">
-            <Login />
-          </div>
-        }
-      />
+      <Route path="/login" element={<Login handleLogin={handleLogin} />} />
       <Route
         path="/register"
         element={
